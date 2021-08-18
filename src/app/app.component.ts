5b64f1e2914f5d179e186c6c5251bd53f4fb17b3
Simobini74 <17884614@studenti.unipi.it>
2021-08-18 10:22:48 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Esercizio Angular';
+  title = 'prova del mio progetto';
 }
