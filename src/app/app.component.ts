618d409a925b5fde1a843c222c97061cadf9db7a
Simobini74 <17884614@studenti.unipi.it>
2021-08-19 08:45:08 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'; 
+import { OpenweatherService } from './openweather.service';
 
 
 class Postit {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OpenweatherService } from './openweather.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [OpenweatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
