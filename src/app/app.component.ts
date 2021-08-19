f4ce6337ca2d23d5f73a0017d689ecc01abea75a
Simobini74 <17884614@studenti.unipi.it>
2021-08-19 08:42:35 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'; 
 
+
 class Postit {
   nome: string;
   data: string;
--- src/app/openweather.service.ts
+++ src/app/openweather.service.ts
@@ -6,7 +6,13 @@ import { HttpClient } from "@angular/common/http";
   providedIn: 'root'
 )
 export class OpenweatherService {
+  apiKEY: string = 'f2e72c0506fe42ca90fe869aeca6b59a'
+  apiURL: string = 'https://api.weatherbit.io/v2.0/current';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getData(CityName: string): Observable<Object> {
+    return this.http.get(this.apiURL+'?key='+this.apiKEY+'&city='+CityName);
+  }
 
 }
