c7d5cd2ee315e814647a2d2891a85ddbd0602d35
Simobini74 <17884614@studenti.unipi.it>
2021-08-20 07:54:59 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,6 @@ import { Component } from '@angular/core';
 import { OpenweatherService } from './openweather.service';
 
 
-class Postit {
-  nome: string;
-  data: string;
-  colore: string;
-  temperatura: string;
-  contenuto: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,36 +9,16 @@ class Postit {
 })
 export class AppComponent {
   title = 'prova progetto';
-  selezionato: Postit; 
-  postit:Array<Postit> = [
-    {
-      nome: 'Torino',
-      data: '14 luglio',
-      colore: 'rosso',
-      contenuto: 'pippo',
-      temperatura:'25 gradi'
-    }, {
-      nome: 'Milano',
-      data: '14 agosto',
-      colore: 'verde',
-      contenuto: 'pluto',
-      temperatura: '30 gradi'
-    }, {
-      nome: 'Genova',
-      data: '18 maggio',
-      colore:'blu',
-      contenuto:'paperino',
-      temperatura:'28 gradi'
-    }
-  ]; 
+  selezionato: string; 
+  temperatura: string;
+  postit:Array<string> = ['Torino','Milano','Genova'];  
+  
   constructor(private wbs: OpenweatherService) { } 
-  refreshTemperature(itemName: string) {
-    var trovato: Array<Postit> = this.postit.filter(
-      el => ( el.nome === itemName )
-    );
-    this.selezionato = trovato[0];
-    this.wbs.getData(this.selezionato.nome).subscribe(
-      ( x: any ) => this.selezionato.temperatura = x.data[0].temp,
+  refreshTemperature(itemName: string) {     
+
+    this.selezionato = itemName;
+    this.wbs.getData(itemName).subscribe(
+      ( x: any ) => this.temperatura = x.data[0].temp,
       err => console.error('Observer got an error: ' + err)
     );
  }
