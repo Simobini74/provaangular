6091dcc775521d33c1ece734ee8ffbf1e17be742
Simobini74 <17884614@studenti.unipi.it>
2021-08-21 08:38:02 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core'; 
 import { OpenweatherService } from './openweather.service';
 
+class cityTemp {
+  luogo: string;
+  valore: string
+}
 
 @Component({
   selector: 'app-root',
@@ -8,22 +12,20 @@ import { OpenweatherService } from './openweather.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prova progetto';
-  selezionato: string; 
-  temperatura: string;
+  title = 'prova progetto';   
   postit:Array<string> = ['Torino','Milano','Genova'];  
-  
+  selezione: cityTemp = new cityTemp();  
   constructor(private wbs: OpenweatherService) { } 
   refreshTemperature(itemName: string) {     
 
-    this.selezionato = itemName;
+    this.selezione.luogo = itemName;
     this.wbs.getData(itemName).subscribe(
-      ( x: any ) => this.temperatura = x.data[0].temp,
+      ( x: any ) => this.selezione.valore = x.data[0].temp,
       err => console.error('Observer got an error: ' + err)
     );
  }
 
   clean() {
-    this.selezionato=undefined;
+    this.selezione=undefined;
   }
 }
